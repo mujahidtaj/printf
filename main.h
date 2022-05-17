bc7a9ce3b71c4163e8141c195b2a3d3c8f4d7750
mujahidtaj <101469273+mujahidtaj@users.noreply.github.com>
2022-05-17 14:14:00 +0200

    Update

--- main.h
+++ main.h
@@ -7,8 +7,6 @@ int main()
    char str[] = "THE SKY IS BLUE";
 
    printf("\nThe value returned by printf() for the above string is : %d", printf("%s", str));
-
    fprintf( stdout, "hello world\n" );
-
    return 0;
 }
